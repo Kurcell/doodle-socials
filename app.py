5470c644d5cd7f2d0d207ee1e5f6119b65438d1e
kpurcell <kpurcell@optivon.net>
2022-10-20 02:06:03 -0400

    feature/JD-19-kpurcell

--- app.py
+++ app.py
@@ -24,13 +24,44 @@ class User(db.Model):
     password = db.Column(db.String(200), nullable=False)
     createdat = db.Column(db.DateTime(200), default = datetime.utcnow)
 
+@dataclass
+class Post(db.Model):
+    __tablename__ = "post"
+
+    pid: int
+    createdat: datetime
+
+    pid = db.Column(db.Integer, primary_key=True)
+    createdat = db.Column(db.DateTime(200), default = datetime.utcnow)
+
+db.create_all()
+db.session.commit()
+
 @app.route('/', methods=['GET'])
 def index():
     return 'Badabing baby.'
 
+@app.route('/user', methods=['POST'])
+def createUser():
+    element = User()
+    db.session.add(element)
+    db.session.commit()
+    return 'Succesfully added user.'
+
 @app.route('/users', methods=['GET'])
 def users():
      return jsonify(User.query.all())
+
+@app.route('/post', methods=['POST'])
+def createPost():
+    element = Post()
+    db.session.add(element)
+    db.session.commit()
+    return 'Succesfully added post.'
+
+@app.route('/posts', methods=['GET'])
+def posts():
+    return jsonify(Post.query.all())
 
 if __name__ == "__main__":
     app.run(debug=True)
