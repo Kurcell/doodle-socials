f60161c89e94d118ec67f71f7004180374ed54af
GrandDad7 <jose.tua@upr.edu>
2022-10-25 21:11:24 -0400

    fixing base app.py, removing dataclass
    adding inits for python console testing
    removing dataclass (not working?)

--- app.py
+++ app.py
@@ -1,27 +1,21 @@
-from dataclasses import dataclass
 from flask import Flask, jsonify, render_template, request, url_for, redirect, current_app
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, ForeignKey, Integer, String, DateTime, Boolean
 from datetime import datetime
 import string
 
-# app_ctx = app.app_context()
-# app_ctx.push()
-# current_app.config["ENV"]
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://gxsfadrohqpnss:010bf417fdce3c2640007c67f4054326277b39d16958a568984054cb6fa8cb24@ec2-18-207-37-30.compute-1.amazonaws.com:5432/d5h4drurgvgtk6'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app_ctx = app.app_context()
+app_ctx.push()
+current_app.config["ENV"]
+
+
 db = SQLAlchemy(app)
 
-# def app_context():
-#     app = Flask(__name__)
-#     with app.app_context():
-#         yield
-
-
-@dataclass
 class User(db.Model):
     __tablename__ = "users"
 
@@ -33,7 +27,12 @@ class User(db.Model):
     createdat = db.Column(db.DateTime(200), default=datetime.utcnow)
     users_post = db.relationship('Post', backref='user')
 
-@dataclass
+    def __init__(self, username, screenname, password, email):
+        self.username = username
+        self.screenname = screenname
+        self.password = password
+        self.email = email
+
 class Post(db.Model):
     __tablename__ = "post"
     
@@ -42,7 +41,11 @@ class Post(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('users.uid'))
     canvas_pid = db.relationship('Canvas', backref='post')
 
-@dataclass 
+    def __init__(self, pid, user_id, content):
+        self.pid = pid
+        self.uid = user_id
+        self.content = content
+ 
 class Canvas(db.Model):
     __tablename__ = "canvas"
     
@@ -50,7 +53,11 @@ class Canvas(db.Model):
     instructions = db.Column(db.String(500), nullable=False) 
     post_id = db.Column(db.Integer, db.ForeignKey('post.pid'))
 
-@dataclass
+    def __init__(self, cid, post_id, instructions):
+        self.cid = cid
+        self.post_id = post_id
+        self.instructions = instructions
+
 class Blocking(db.Model):
     __tablename__ = "blocking"
 
@@ -58,13 +65,22 @@ class Blocking(db.Model):
     blockee_uid = db.Column(db.Integer, nullable=False)
     blocked_uid = db.Column(db.Integer, nullable=False)
 
-@dataclass
+    def __init__(self, blocking_id, blockee_uid, blocked_uid):
+        self.blocking_id = blocking_id
+        self.blockee_uid = blockee_uid
+        self.blocked_uid = blocked_uid
+
 class Following(db.Model):
     __tablename__ = "following"
 
     following_id = db.Column(db.Integer, primary_key=True) # to be used for query optimazation
     followee_uid = db.Column(db.Integer, nullable= False)
     followed_uid = db.Column(db.Integer, nullable= False)
+
+    def __init__(self, following_id, followee_uid, followed_uid):
+        self.following_id = following_id
+        self.followee_uid = followed_uid
+        self.followed_uid = followed_uid
 
 
 @app.route("/users", methods=['GET'])
