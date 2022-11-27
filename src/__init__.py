2ddc9c941ea44f441623c251ae2ad33562a8a78f
root <root@DESKTOP-2BUQ2A6.localdomain>
2022-11-26 21:23:53 -0500

    beginning changes to jwt

--- src/__init__.py
+++ src/__init__.py
@@ -1,24 +1,41 @@
-import os
-from flask import Flask, current_app
+from functools import wraps
+import os, jwt
+from flask import Flask, current_app, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_swagger_ui import get_swaggerui_blueprint
 from flask_cors import CORS
-from flask_login import LoginManager
+# from flask_login import LoginManager
 
 from dotenv import load_dotenv
-
 load_dotenv()
+
+def token_required(func):
+    # decorator factory which invoks update_wrapper() method and passes decorated function as an argument
+        @wraps(func)
+        def decorated(*args, **kwargs):
+            token = request.args.get('token')
+            if not token:
+                return jsonify({'Alert!': 'Token is missing!'}), 401
+
+            try:
+                data = jwt.decode(token, app.config['SECRET_KEY'])
+            # You can use the JWT errors in exception
+            # except jwt.InvalidTokenError:
+            #     return 'Invalid token. Please log in again.'
+            except:
+                return jsonify({'Message': 'Invalid token'}), 403
+            return func(*args, **kwargs)
+        return decorated
 
 # Database setup
 db = SQLAlchemy()
-
 
 def create_app():
     from . import models, routes
 
     app = Flask(__name__, instance_relative_config=False)
     CORS(app, supports_credentials=True)
-    
+
     SWAGGER_URL = '/swagger'
     API_URL = '/static/swagger.json'
     swaggerui_blueprint = get_swaggerui_blueprint(
@@ -30,25 +47,23 @@ def create_app():
     )
     app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
     
-    app.secret_key = "doodle-secret-api"
+    app.config['SECRET_KEY'] = os.getenv('DATABASE_URL')
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL').replace("postgres://", "postgresql://", 1)
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app_ctx = app.app_context()
     app_ctx.push()
     current_app.config["ENV"]
 
-    login_manager = LoginManager()
-    login_manager.init_app(app)
+    # login_manager = LoginManager()
+    # login_manager.init_app(app)
 
     models.init_app(app)
     routes.init_app(app)
 
-    from .models.models import User
-    @login_manager.user_loader
-    def load_user(user_id):
-        return User.query.get(int(user_id))
-    
-
+    # from .models.models import User
+    # @login_manager.user_loader
+    # def load_user(user_id):
+    #     return User.query.get(int(user_id))  
 
     # with app.app_context():
     #     from src.models.models import User, Post, Blocking, Following
--- src/routes/auth.py
+++ src/routes/auth.py
@@ -1,8 +1,9 @@
+import os, jwt, datetime
 from flask import Blueprint, request, jsonify
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import login_user, login_required, logout_user, current_user
+# from flask_login import login_user, token_required, logout_user, current_user
 from ..models.models import User
-from ..__init__ import db
+from ..__init__ import token_required
 
 auth_bp = Blueprint('auth', __name__)
 
@@ -11,42 +12,59 @@ def hello():
     return "Welcome to Dudol's Social API!"
 
 @auth_bp.route("/hello", methods=['GET'])
-@login_required
+@token_required
 def helloUser():
-    return "Hello, " + current_user.username
+    payload = jwt.decode(request.args.get('token'), os.getenv('SECRET_KEY'))
+    return jsonify(payload)
+    # return "Hello, " + current_user.username
 
 @auth_bp.route("/verify", methods=['GET'])
-@login_required
+@token_required
 def verify():
-    return jsonify(user = {
-            'uid': current_user.uid,
-            'username' : current_user.username,
-            'screenname' : current_user.screenname
-            })
+    return ""
+    # return jsonify(user = {
+    #         'uid': current_user.uid,
+    #         'username' : current_user.username,
+    #         'screenname' : current_user.screenname
+    #         })
 
 @auth_bp.route('/login', methods=['POST'])
 def login():
-    if current_user.is_authenticated:
-        return {
-            'message': 'User currently logged in.'
-        }
-
+    auth = request.authorization
     req = request.get_json()
     email = req['email']
     password = req['password']
-    remember = True if req['remember'] else False
-
     user = User.query.filter_by(email=email).first()
-
     if user and check_password_hash(user.password, password):
-        login_user(user, remember)
-        return {
-            'id': user.uid
-        }
+        token = jwt.encode({
+        'user': {'uid':user.uid, 'username': user.username, 'screename': user.screenname}, 
+        'exp': str(datetime.datetime.utcnow() + datetime.timedelta(minutes=30))
+        }, 
+        os.getenv('SECRET_KEY'))
+        return {'token': token}
+    return {'message': 'Invalid credentials.'}
+    
+    # if current_user.is_authenticated:
+    #     return {
+    #         'message': 'User currently logged in.'
+    #     }
+
+    # req = request.get_json()
+    # email = req['email']
+    # password = req['password']
+    # remember = True if req['remember'] else False
+
+    # user = User.query.filter_by(email=email).first()
+
+    # if user and check_password_hash(user.password, password):
+    #     login_user(user, remember)
+    #     return {
+    #         'id': user.uid
+    #     }
 
     return {
             'message': 'Please check your login credentials and try again.'
-        }
+    }
 
 @auth_bp.route('/register', methods=['POST'])
 def register():
@@ -60,16 +78,16 @@ def register():
     # create new user with form data, password hashed so it isn't plain text
     new_user = User.create(username, screenname, profile, password, email)
 
-    login_user(new_user)
+    # login_user(new_user)
     
     return {
             'id': new_user.uid
     }, 201
 
 @auth_bp.route('/logout')
-@login_required
+@token_required
 def logout():
-    logout_user()
+    # logout_user()
     return {
             'message':  'Logging out.'
     }
